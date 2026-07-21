7fdd6b654a23ae27f1bda164c16e37fa91cdcb12
Kashvi192007 <kashvi.25005112@kiet.edu>
2026-07-22 00:10:59 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -33,6 +33,7 @@ export default async function handler(req, res) {
         );
 
         const data = await response.json();
+        console.log(JSON.stringify(data, null, 2));
 
         const reply =
             data?.candidates?.[0]?.content?.parts?.[0]?.text ||
